43092625a13d9777c7e1b57b0ae0f794f6e58946
Kartikay Sharma <sharmakartikay786@gmail.com>
2024-08-25 12:08:47 +0530

    added cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors  = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post('/bfhl', (req, res) => {
     const { data } = req.body;
